9c56a00d27f2f37ce848d4ed2f2a89dbef9d4f3b
Carolene Wilson <carolenesw@gmail.com>
2020-07-22 12:56:02 -0400

    create and populate seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+--   write insert queries to populate burger_db 
+INSERT INTO burgers (burger_name) VALUES ('Double-Quarter_Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter_Pounder', true);
