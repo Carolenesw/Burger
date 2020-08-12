55da184cdaae88dd2819df47425beb4fb549b626
Carolene Wilson <carolenesw@gmail.com>
2020-08-12 15:23:30 -0400

    create click event ajax call to devoured burger

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,21 +1,38 @@
-$(function (){
+$(function () {
+  // on click event capture new burger name
+  $(".create-form").on("submit", function (event) {
+    // preventDefault on a submit event.
+    event.preventDefault();
 
-    $(".create-form").on("submit", function(event) {
-      // preventDefault on a submit event.
-      event.preventDefault();
-      
-      let newBurger= {
-          burger_name: $("#burger_input").val().trim()
-      };
-  
-      // Send the POST request 
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(function(res) {
-          // Reload the page to get the updated list
-          console.log
-          location.reload();
-        });
+    let newBurger = {
+      burger_name: $("#burger_input").val().trim(),
+    };
+
+    // Send the POST request for new burger object
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function (res) {
+      // Reload the page to get the updated list
+      console.log;
+      location.reload();
     });
-})
+  });
+
+  //   create on click to change burger state/status
+  $(".dev_burger").on("click", function (event) {
+    let id = $(this).data("id");
+    let newState = {
+      devoured: 1,
+    };
+
+    // use PUT request to update burger status
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newState,
+    }).then(function () {
+
+    });
+    location.reload();
+  });
+});
