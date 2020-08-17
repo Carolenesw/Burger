9d973974b0cc697099705acffaabeed3d6bfc416
Carolene Wilson <carolenesw@gmail.com>
2020-08-17 14:48:08 -0400

    add JAWS connection

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,31 @@
 const mysql = require("mysql");
 
-// set up connection to mysql server and database
-const connection = mysql.createConnection({
+// set up database connection using JAWSDB_URL
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  // // set up connection to sql server and database
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "mysql",
-    database: "burgers_db",
+    password: "password",
+    database: "employee_db",
   });
-  
+}
+
+
   // create function to handle connection
-  connection.connect(function (err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-    }
+  connection.connect()
   
-    console.log("DB connection established id: " + connection.threadId);
+  // connection.connect(function (err) {
+  //   if (err) {
+  //     console.error("error connecting: " + err.stack);
+  //   }
   
-    });
+  //   console.log("DB connection established id: " + connection.threadId);
+  
+  //   });
 
 
 // Export connection for our ORM to use.
